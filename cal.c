fad80b3f5d560017de7e918a5b1ee8b79d4dae45
sky <skycomputer744@gmail.com>
2021-02-06 14:29:29 +0530

    update

--- cal.c
+++ cal.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	printf("hello git");
-	ptintf("anything new ");
-	printf("yes")
+	printf("hello git\n");
+
+	int a,b,ans;
+	
+	printf("enter two number to add : ");
+	scanf("%d%d",&a,&b);
+	ans=a+b;
+	printf("%d + %d = %d",a,b,ans);
 	return 0;
 	
 }
