1c909fb5b1e6eee42334ee3bd8484a22e866c0fb
Kishan kakadiya <78640556+kpkakadiya@users.noreply.github.com>
2021-02-08 16:02:36 +0530

    update cal.c

--- cal.c
+++ cal.c
@@ -2,11 +2,20 @@
 int main()
 {
 	printf("hello git\n");
-	int a,b,ans;
+	int a,b,ans,choice;
+	printf("1. add \n2. substrat");
+	printf("enter choice : ");
+	scanf("%d",&choice);
+	if(choice == 1){
 	printf("enter two number to add : ");
 	scanf("%d%d",&a,&b);
 	ans=a+b;
-	printf("%d + %d = %d",a,b,ans);
+	printf("%d + %d = %d",a,b,ans);}
+	else{
+	printf("enter two number to add : ");
+	scanf("%d%d",&a,&b);
+	ans=a+b;
+	printf("%d - %d = %d",a,b,ans);	}
 	return 0;
 	
 }
